e390d80d790412b1726419a771c367d74002c373
AlexSmek <uss.north.caroline@gmail.com>
2023-12-14 11:54:16 +0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="frame">
+        <img src="@/imges/background.jpg" alt="" class="backroundImg">
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+@import "@/style.css";
+.frame{
+}
+.backroundImg{
+}
+</style>
---
+++ src/components/contentBlock.vue
@@ -0,0 +1,23 @@
+<template>
+
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'App',
+    components: {
+    },
+    methods: {
+      scroll(id) {
+        const el = document.getElementById(id);
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  @import "@/style.css";
+  </style>
+  
